e9c26f27f6f78b9a5fb242dc66a62f24e3d83b4a
panjiachen <panfree23@gmail.com>
2015-02-25 14:04:58 +0800

    init

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var path = require("path");
+var media = path.join(__dirname,"../public/media");
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    var fs = require("fs");
+    fs.readdir(media,function(err,names){
+        if(err){
+            console.log(err);
+        }else{
+            res.render('index', { title: 'Music-Visualization', music: names });
+        }
+    })
+  
+});
+
+module.exports = router;

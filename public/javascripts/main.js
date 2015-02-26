870096abacab6f90c65b2ee5e79c41249d61d6a6
panjiachen <panfree23@gmail.com>
2015-02-26 14:52:11 +0800

    reactive

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,155 +1,115 @@
-var musicList=$("#list");
-var source=null;
-var xhr = new XMLHttpRequest();
-var count=0;
-var isAnalysis=false;
-window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.msAudioContext;//兼容游览器
-window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame;
-var start = null;
-var audioContext=new window.AudioContext();
-var gainNode=audioContext[audioContext.createGain?"createGain":"createGainNode"]();
-var analyser=audioContext.createAnalyser();
+var size = 64;
+var canvasHeight;
+var canvasWidth;
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+var line;
+var dots = [];
 
-gainNode.connect(audioContext.destination);
-var size=128;
-analyser.fttSize=size*2;
-analyser.connect(gainNode);
+$('#box').append(canvas);
 
-musicList.on("click","li",function(){
-    $(this).siblings().removeClass('selected');
-    $(this).addClass('selected');
-    loadMusic('media/'+$(this).attr('title'));
+//初始化MusicVisualizer
+var mv = new MusicVisualizer({
+    size: size,
+    visualizer: draw
 })
 
+//选择歌曲
+$("#list").on("click", "li", function() {
+    $(this).siblings().removeClass('selected');
+    $(this).addClass('selected');
+    mv.play('media/' + $(this).attr('title'));
+})
 
-
-function loadMusic(url){
-    var n=++count;
-    source&&source[source.stop? "stop" : "noteOff"]();
-    xhr.abort();
-    xhr.open("GET",url);
-    xhr.responseType="arraybuffer";//配置数据返回类型 arraybuffer 音频以二进制传输
-
-   xhr.onload=function(){//请求成功 
-         if(n != count){
-                return;
-            }
-        audioContext.decodeAudioData(xhr.response,function(buffer){
-            if(n != count){
-                return;
-            }
-            var  bufferSource=audioContext.createBufferSource();//创建一个音频source
-            bufferSource.buffer=buffer;//告诉source是哪个音频
-            bufferSource.connect(analyser);//将source和context's destination连接 相当于扬声器
-            bufferSource[bufferSource.start?"start":"noteOn"](0);//noteOn兼容老版本
-            source=bufferSource;
-            visualizer();
-        },function(err){
-            console.log(err)
-        })
-   }
-   xhr.send();
-}
-
-//改变音量大小
-function changevolume(targetvolume){
-    gainNode.gain.value=targetvolume;
-}
-//分析音频
-function visualizer(){
-    var arr=new Uint8Array(analyser.frequencyBinCount);
-    // analyser.getByteFrequencyData(arr);
-    
-    function show(){
-        analyser.getByteFrequencyData(arr);
-        draw(arr);
-        window.requestAnimationFrame(show);
-    };
-     window.requestAnimationFrame(show);
-};
-
-
+//变化音量
 $("#volume").on('mousedown', function(event) {
-    if(!isAnalysis){
-        visualizer();
-        isAnalysis=true;
-    }
     $(this).on('mousemove', function(event) {
-        changevolume($(this).val()/100);
+        mv.changevolume($(this).val() / 100);
     });
 });
 
-var canvasHeight;
-var canvasWidth;
-var canvas=document.createElement("canvas");
-var line;
-var dots=[];
-var ctx=canvas.getContext("2d");
-$('#box').append(canvas);
-function random(min,max){
-    return Math.round(Math.random()*(max-min)+min);
-}
-function getDots(){
-    dots=[];
-    for(var i=0;i<size;i++){
-        var x=random(0,width); 
-        var y=random(0,height);
-        var color="rgb("+random(0,255)+","+random(0,255)+","+random(0,255)+")"; 
+//随机函数
+function random(min, max) {
+        return Math.round(Math.random() * (max - min) + min);
+    }
+    //随机生成点
+function getDots() {
+    dots = [];
+    for (var i = 0; i < size; i++) {
+        var x = random(0, width);
+        var y = random(0, height);
+        var color = "rgba(" + random(0, 255) + "," + random(0, 255) + "," + random(0, 255) +","+ 0.5+")";
         dots.push({
-            x : x,
-            y : y,
-            color : color
-        });   
+            x: x,
+            y: y,
+            dx:random(1,2),
+            color: color,
+            cap:0
+        });
     }
 }
 
-draw.type="column";//初始化draw的类型
-function draw(arr){
-    ctx.clearRect(0,0,width,height)
-    if(draw.type=='column'){
-        line.addColorStop(0,"red");
-        line.addColorStop(0.5,"yellow");
-        line.addColorStop(1,"green");
-        ctx.fillStyle=line;
-        var w=width/size;
-        for(var i=0;i<size;i++){
-            var h=arr[i]/256*height;
-            ctx.fillRect(w*i,height-h,w*0.6,h)
+draw.type = "column"; //初始化draw的类型
+function draw(arr) {
+    ctx.clearRect(0, 0, width, height);
+     
+    if (draw.type == 'column') {
+        line.addColorStop(0, "red");
+        line.addColorStop(0.5, "yellow");
+        line.addColorStop(1, "green");
+        ctx.fillStyle = line;
+        var w = width / size;
+        var cw=w*0.6;
+        var capH=cw>10?cw:10;
+        for (var i = 0; i < size; i++) {
+            var h = arr[i] / 256 * height;
+            var o = dots[i];
+            ctx.fillRect(w * i, height - h, cw, h);
+            ctx.fillRect(w * i, height - o.cap-capH, cw, capH);
+            o.cap--;
+            if(o.cap<0){
+                o.cap=0;
+            }
+            if(h>0&&o.cap<(h+40)){
+                o.cap=h+40>height-capH?height-capH:h+40;
+            }
         }
-    }else{
-        for(var i=0;i<size;i++){
+    } else {
+        for (var i = 0; i < size; i++) {
             ctx.beginPath();
-            var o=dots[i];
-            var r=arr[i]/256*50;
-            ctx.arc(o.x,o.y,r,0,Math.PI*2,true);
-            var circle=ctx.createRadialGradient(o.x, o.y, 0, o.x, o.y, r);
-            circle.addColorStop(0,"#fff");
+            var o = dots[i];
+            var r = 10+arr[i] / 256 * (height>width?width:height)/10;
+            ctx.arc(o.x, o.y, r, 0, Math.PI * 2, true);
+            var circle = ctx.createRadialGradient(o.x, o.y, 0, o.x, o.y, r);
+            circle.addColorStop(0, "#fff");
             circle.addColorStop(1, o.color);
-            ctx.fillStyle=circle;
+            ctx.fillStyle = circle;
             ctx.fill();
-         }
+            o.x+=o.dx;
+            o.x=o.x>width?0:o.x;
+        }
     }
-    
+
 }
 
 //选择可视化的类型
-$('#typeList').on("click","li",function(){
+$('#typeList').on("click", "li", function() {
     $(this).siblings().removeClass('selected');
     $(this).addClass('selected');
-    draw.type=$(this).attr('data-type');
+    draw.type = $(this).attr('data-type');
 })
 
-function reSize(){
-    height=$('#box').height();
-    width=$('#box').width();
-    canvas.height=height;
-    canvas.width=width;
-    line=ctx.createLinearGradient(0,0,0,height);
-    
+function reSize() {
+    height = $('#box').height();
+    width = $('#box').width();
+    canvas.height = height;
+    canvas.width = width;
+    line = ctx.createLinearGradient(0, 0, 0, height);
+
     getDots();
 };
 reSize()
 
 $(window).resize(function() {
-  reSize()
+    reSize()
 });
---
+++ public/javascripts/musicVisualizer.js
@@ -0,0 +1,76 @@
+window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.msAudioContext;//兼容游览器
+window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame;
+MusicVisualizer.ac=new window.AudioContext();
+
+function MusicVisualizer(obj){
+    var source=null;
+    this.count=0;
+    this.analyser=MusicVisualizer.ac.createAnalyser();
+    this.size=obj.size;
+    this.analyser.fftSize=this.size*2;
+    this.gainNode=MusicVisualizer.ac[MusicVisualizer.ac.createGain?"createGain":"createGainNode"](0);
+    this.gainNode.connect(MusicVisualizer.ac.destination);
+    this.analyser.connect(this.gainNode);
+    this.xhr = new XMLHttpRequest();
+    this.visualizer=obj.visualizer;
+   
+}
+
+
+MusicVisualizer.prototype.load = function(url,fun) {
+    this.xhr.abort();
+    this.xhr.open("GET", url);
+    this.xhr.responseType = "arraybuffer"; //配置数据返回类型 arraybuffer 音频以二进制传输
+    var self=this;
+
+    this.xhr.onload = function() { //请求成功 
+        fun(self.xhr.response);
+    }
+    this.xhr.send();
+}
+
+MusicVisualizer.prototype.decode=function(arraybuffer,fun){
+    MusicVisualizer.ac.decodeAudioData(arraybuffer,function(buffer){
+        fun(buffer)
+    },function(err){
+        console.log(err);
+    })
+}
+MusicVisualizer.prototype.play=function(url){
+     this.visualize();
+    var n=++this.count;
+    var self=this;
+    this.source&&this.stop();
+    this.load(url,function(arraybuffer){
+        if(n != self.count){
+                return;
+            }
+        self.decode(arraybuffer,function(buffer){
+            var  bufferSource=MusicVisualizer.ac.createBufferSource();//创建一个音频source
+            bufferSource.buffer=buffer;//告诉source是哪个音频
+            bufferSource.connect(self.analyser);//将source和context's destination连接 相当于扬声器
+            bufferSource[bufferSource.start?"start":"noteOn"](0);//noteOn兼容老版本
+            self.source=bufferSource;
+        })
+    })
+}
+MusicVisualizer.prototype.stop=function(){
+    this.source[this.source.stop? "stop" : "noteOff"](0);
+}
+//改变音量大小
+MusicVisualizer.prototype.changeVolume=function(targetvolume){
+   this.gainNode.gain.value=targetvolume;
+}
+//分析音频
+MusicVisualizer.prototype.visualize = function() {
+    var arr = new Uint8Array(this.analyser.frequencyBinCount); //长度
+    var self = this;
+
+    function show() {
+        self.analyser.getByteFrequencyData(arr);
+        // console.log(arr)
+        self.visualizer(arr);
+        window.requestAnimationFrame(show);
+    };
+    window.requestAnimationFrame(show);
+}

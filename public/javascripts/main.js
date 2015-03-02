05a6d5966bdd411477fa7cb3174fd5216a061f93
panjiachen <panfree23@gmail.com>
2015-03-02 15:52:37 +0800

    3d

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,7 +6,6 @@ var ctx       = canvas.getContext("2d");
 var line;
 var dots      = [];
 var colorList = ["gold","cyan","magenta"];
-
 $('#box').append(canvas);
 
 //初始化MusicVisualizer
@@ -14,6 +13,88 @@ var mv = new MusicVisualizer({
     size: size,
     visualizer: draw
 })
+
+/************************************************************************/
+/* 3d*/
+var scene=new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(45, canvasWidth / canvasHeight, 0.1, 1000);
+render = new THREE.WebGLRenderer({antialias: true});
+render.setSize(window.canvasWidth, window.canvasHeight);
+document.body.appendChild(render.domElement);
+var MWIDTH = 2;
+var MTHICKNESS = 1;
+var GAP = 1;
+var METERNUM = Math.round(100 / (this.MWIDTH + this.GAP)); // calculated by 200/(MWIDTH+GAP),200 is the width of the visualizer area
+//创建绿色柱条的形状
+var cubeGeometry = new THREE.CubeGeometry(MWIDTH, 1, MTHICKNESS);
+//创建绿色柱条的材质
+var cubeMaterial = new THREE.MeshPhongMaterial({
+    color: 0x01FF00,
+    ambient: 0x01FF00,
+    specular: 0x01FF00,
+    shininess: 20,
+    reflectivity: 5.5
+});
+//创建白色盖子的形状
+var capGeometry = new THREE.CubeGeometry(MWIDTH, 0.5, MTHICKNESS);
+//创建白色盖子的材质
+var capMaterial = new THREE.MeshPhongMaterial({
+    color: 0xffffff,
+    ambient: 0x01FF00,
+    specular: 0x01FF00,
+    shininess: 20,
+    reflectivity: 5.5
+});
+
+//创建一字排开的柱条和盖子，并添加到场景中
+for (var i = METERNUM - 1; i >= 0; i--) {
+    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cube.position.x = -45 + (MWIDTH + GAP) * i;
+    cube.position.y = -1;
+    cube.position.z = 0.5;
+    cube.castShadow = true;
+    cube.name = 'cube' + i;
+    scene.add(cube);
+    var cap = new THREE.Mesh(capGeometry, capMaterial);
+    cap.position.x = -45 + (MWIDTH + GAP) * i;
+    cap.position.y = 0.5;
+    cap.position.z = 0.5;
+    cap.castShadow = true;
+    cap.name = 'cap' + i;
+    scene.add(cap);
+};
+ var renderAnimation = function() {
+    if (mv.analyser) {
+        var array = new Uint8Array(mv.analyser.frequencyBinCount);
+        mv.analyser.getByteFrequencyData(array);
+        var step = Math.round(array.length / METERNUM);
+        for (var i = 0; i < METERNUM; i++) {
+            var value = array[i * step] / 4;
+            value = value < 1 ? 1 : value;
+            var meter = scene.getObjectByName('cube' + i, true),
+                cap = scene.getObjectByName('cap' + i, true);
+            meter.scale.y = value;
+            //计算柱条边沿尺寸以获得高度
+            meter.geometry.computeBoundingBox();
+            height = (meter.geometry.boundingBox.max.y - meter.geometry.boundingBox.min.y) * value;
+            //将柱条高度与盖子高度进行比较
+            if (height / 2 > cap.position.y) {
+                cap.position.y = height / 2;
+            } else {
+                cap.position.y -= controls.dropSpeed;
+            };
+        }
+    };
+    //重新渲染画面
+    render.render(scene, camera);
+    requestAnimationFrame(renderAnimation);
+};
+requestAnimationFrame(renderAnimation);
+/************************************************************************/
+
+
+
+
 
 //选择歌曲
 $("#list").on("click", "li", function() {
@@ -44,7 +125,7 @@ function random(min, max) {
 // }
 //随机生成点
 function getDots() {
-    dots = [];
+    dots = []
     for (var i = 0; i < size; i++) {
         var x = random(0, width);
         var y = random(0, height);
